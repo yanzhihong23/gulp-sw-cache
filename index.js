194e21cba8c1b9cc51c97e8fa92387047192ecc0
Hom Yan <yanzhihong23@qq.com>
2016-06-08 13:05:19 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+"use strict";
+
+var ejs       = require('ejs'),
+    fs        = require('fs'),
+    through   = require('through'),
+    gutil     = require('gulp-util'),
+    path      = require('path'),
+    minimatch = require('minimatch'),
+    slash     = require('slash'),
+    crypto    = require('crypto');
+
+
+function sw(options) {
+  var filename,
+      versionPrefix,
+      version,
+      include,
+      exclude,
+      files = [],
+      cwd;
+
+  options = options || {};
+
+  versionPrefix = options.versionPrefix || 'sw-';
+  version = versionPrefix + crypto.createHash('sha256').digest("hex");
+
+  filename = options.filename || 'sw.js';
+  include = Array.prototype.concat(options.include || []);
+  exclude = Array.prototype.concat(options.exclude || []).concat(include);
+  cwd = process.cwd();
+
+  function shouldExcludeFile(filePath) {
+    return exclude.some(minimatch.bind(null, filePath));
+  }
+
+  function append(file) {
+    var filepath;
+
+    if (file.isNull())  return;
+    if (file.isStream()) return this.emit('error', new gutil.PluginError('gulp-manifest',  'Streaming not supported'));
+
+    filepath = slash(file.relative);
+
+    if (shouldExcludeFile(filepath)) {
+      return;
+    }
+
+    console.log('cache file: ' + filepath);
+
+    files.push(filepath);
+  }
+
+  function end() {
+    var template = fs.readFileSync('template/sw.js', 'utf8');
+    var content = ejs.render(template, {version: version, versionPrefix: versionPrefix, files: files});
+
+    var swFile = new gutil.File({
+      cwd: cwd,
+      base: cwd,
+      path: path.join(cwd, filename),
+      contents: new Buffer(content)
+    });
+
+    template = fs.readFileSync('template/sw.controller.js', 'utf8');
+    content = ejs.render(template, {filename: filename});;
+
+    var ctrlFile = new gutil.File({
+      cwd: cwd,
+      base: cwd,
+      path: path.join(cwd, 'sw.controller.js'),
+      contents: new Buffer(content)
+    });
+
+    this.emit('data', swFile);
+    this.emit('data', ctrlFile);
+    this.emit('end');
+  }
+
+  return through(append, end);
+}
+
+module.exports = sw;
---
+++ template/sw.js
@@ -0,0 +1,39 @@
+var staticCacheName = '<%= version %>';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll(<%- JSON.stringify(files) %>);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('<%= versionPrefix %>') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
